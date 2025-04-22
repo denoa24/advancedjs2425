44a3b47761e5636a01e5077130ea736c52373391
denoa24 <denis.vna24@gmail.com>
2025-04-22 21:42:39 +0300

    curs1

---
+++ curs1/Carte.js
@@ -0,0 +1,25 @@
+class Carte {
+    constructor(titluCarte, autorCarte, edituraCarte, anCarte, pretCarte) {
+        this.titlu = titluCarte;
+        this.autor = autorCarte;
+        this.editura = edituraCarte;
+        this.an = anCarte;
+        this.pret = pretCarte;
+    }
+    obtineTitlu(){
+        console.log(`Titlul cartii este ${this.titlu}`)
+    }
+    afiseazaInfo(){
+        return `Detalii despre aceasta carte sunt: ${this.titlu}, ${this.autor}, ${this.pret}`;
+    }
+    deschidCartea() {
+        console.log('Am deschis cartea la pagina 15');
+    }
+    randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+
+}
+
+export { Carte };
